fbabefbef1fdf467ad20f31bb5486f0e4ac5f578
SangWoo Kim <sangwoo1111@gmail.com>
2017-08-17 08:14:20 -0700

    populating

--- public/js/jq.js
+++ public/js/jq.js
@@ -1,17 +1,53 @@
-
-
 $(function () {
-    console.log("ajax running")
-    $.ajax({
-        type: 'GET',
-        url: 'api/hotels',
-        success: function(hotels) {
-            console.log("success!")
-            $.each(hotels, function(i, hotel) {
-                console.log(hotel.name)
-            })
-        },
-        error: console.log("there is an error")
+    let $hotels = $('#hotel-choices');
+    const $restaurants = $('#restaurant-choices');
+    const $activities = $('#activity-choices');
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'api/hotels',
+    //     dataType: 'json',
+    //     success: function(hotels) {
+    //         $.each(hotels, function(i, hotel) {
+    //             var $option = $('<option></option>')
+    //             .text(hotel.name)
+    //             .val(hotel.id);
+    //             // $hotels.append($option);
+    //             console.log($hotels)
+    //         })
+    //     },
+    //     error: console.log("there is an error")
+    // })
+
+    // $.get('/api/hotels')
+    // .then(function (hotels) {
+    //     $.each(hotels, function(i, hotel) {
+    //         var $option = $('<option></option>')
+    //         .text(hotel.name)
+    //         .val(hotel.id);
+    //         $hotels.append($option);
+    //     });
+    // })
+    // .catch( console.error.bind(console) );
+
+
+
+
+    const promiseArray = [$.get('/api/hotels'), $.get('/api/restaurants'), $.get('/api/activities')];
+
+    Promise.all(promiseArray)
+    .then(([hotels, restaurants, activities]) => {
+        output(hotels, $hotels);
+        output(restaurants, $restaurants);
+        output(activities, $activities);
     })
+
+    function output (items, selector) {
+        $.each(items, function(i, item) {
+            var $option = $('<option></option>')
+            .text(item.name)
+            .val(item.id);
+            selector.append($option);
+        });
+    }
 
 })
